6d05ce519eed020ef4175404bf0a4b0512624ccb
Puspak Sengupta <puspaksg1994@gmail.com>
2026-07-04 23:53:28 +0530

    Second commit

---
+++ src/test/java/com/commerce/email_delivery_service/EmailDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.email_delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
